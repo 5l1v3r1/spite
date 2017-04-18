6ab5d4c0d4128f5b146a22bda9a252b029503d93
Robert Pavel <rspavel@lanl.gov>
2017-04-18 16:38:24 -0500

    Finished Migration of Process Args

--- MPI_Reference/MainDriver.cpp
+++ MPI_Reference/MainDriver.cpp
@@ -1,71 +1,13 @@
 #include "TaskGranularityProxy.hpp"
 #include "SyntheticTask.hpp"
+#include "ProcessArgs.hpp"
 
 #include <mpi.h>
-#include <cstring>
-#include <cstdlib>
 #include <chrono>
 
-#include <iostream>
 #include <fstream>
 
-namespace SPTE_Proxy
-{
-
 const int GENERIC_MESSAGE_TAG = 71;
-
-SPTE_Proxy::RunConfig processArgs(int argc, char ** &argv)
-{
-	RunConfig retVal;
-	//Set default values
-	retVal.dim = 1;
-	retVal.depSize = 16;
-	retVal.taskSize = 128;
-	retVal.numIters = 16;
-
-	//For each argument
-	for(int i = 1; i < argc; i++)
-	{
-		//Make a copy of the string that we can destroy
-		char argb[256];
-		strcpy(argb, argv[i]);
-		//Split string to collect the name and value
-		char * argname = std::strtok(argv[i], "=");
-		if(std::strrchr(argname, '-') != nullptr)
-		{
-			argname = std::strrchr(argname, '-')+1;
-		}
-		char * argval = std::strtok(nullptr, "=");
-		//Now for a simple switch to set values
-		if(strcmp(argname, "help") == 0)
-		{
-			std::cout << "-dim=[integer number of dimensions in cartesian space]" << std::endl;
-			std::cout << "-arg=[integer size of arguments/dependencies in chars]" << std::endl;
-			std::cout << "-task=[integer size of one dimension of task matrix]" << std::endl;
-			std::cout << "-iters=[number of iterations of test to run for averaged time]" << std::endl;
-		}
-		if(argval != nullptr)
-		{
-			if(strcmp(argname, "dim") == 0)
-			{
-				retVal.dim = atoi(argval);
-			}
-			else if(strcmp(argname, "arg") == 0)
-			{
-				retVal.depSize = atoi(argval);
-			}
-			else if(strcmp(argname, "task") == 0)
-			{
-				retVal.taskSize = atoi(argval);
-			}
-			else if(strcmp(argname, "iters") == 0)
-			{
-				retVal.numIters = atoi(argval);
-			}
-		}
-	}
-	return retVal;
-}
 
 void wait_comms(int dim, MPI_Request *reqBuffer, MPI_Comm &cartComm)
 {
@@ -112,13 +54,10 @@ void comm_neighbors(int dim, int cartRank, double ** buffers, unsigned int bufSi
 	return;
 }
 
-}
 
 int main(int argc, char ** argv)
 {
 	using SPTE_Proxy::processArgs;
-	using SPTE_Proxy::comm_neighbors;
-	using SPTE_Proxy::wait_comms;
 
 	//Initialize MPI
 	MPI_Init(&argc, &argv);
---
+++ SPTE_lib/ProcessArgs.hpp
@@ -0,0 +1,12 @@
+#ifndef PROCESSARGS_HPP
+#define PROCESSARGS_HPP
+
+#include "TaskGranularityProxy.hpp"
+
+namespace SPTE_Proxy
+{
+SPTE_Proxy::RunConfig processArgs(int argc, char ** &argv);
+}
+
+#endif
+
